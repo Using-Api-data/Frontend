55dc89a0ce35f5818306beeba3d3a2f0dc0ae4ec
nayounsang <nayounsang722@gmail.com>
2023-06-25 21:54:55 +0900

    1st

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import KakaoMap from './component/kakaomap/KakaoMap';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <KakaoMap/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/const/mapkey.js
@@ -0,0 +1,1 @@
+export const mapkey = "5089ad092ba73c83f5c8c1bf35117076";
