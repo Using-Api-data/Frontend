e34dfe86c546268f930d11a54cb856e776599cf8
nayounsang <77400131+nayounsang@users.noreply.github.com>
2023-07-19 20:17:44 +0900

    Delete OAuth.js

--- src/const/OAuth.js
+++
@@ -1,6 +0,0 @@
-import { back } from "./backend";
-
-export const CLIENT_ID = "4914d3841f3d777c1b8ff3ac3866a0d8";
-const REDIRECT =  "oauth2/authorization/kakao";
-
-export const KAKAO_AUTH_URL = back + REDIRECT;
